9b21fe6ffded057e21958f38d6cad42ed4ce8ed6
MaksimZotov <zotovm256@gmail.com>
2024-12-07 00:44:40 -0800

    Check Koin Annotations with two modules with includes

--- core/app/src/commonMain/kotlin/com/mz/kmpstudy/core/app/koin/ModulesProvider.kt
+++ core/app/src/commonMain/kotlin/com/mz/kmpstudy/core/app/koin/ModulesProvider.kt
@@ -6,15 +6,19 @@ import com.mz.kmpstudy.core.routing.di.coreRoutingModule
 import com.mz.kmpstudy.feature.counter.featureCounterModule
 import com.mz.kmpstudy.feature.home.di.featureHomeModule
 import org.koin.core.module.Module
+import org.koin.dsl.module
 
 object ModulesProvider {
     fun getModules(): List<Module> {
         return listOf(
             coreRoutingModule,
             coreAppModule,
-            clientCounterModule,
             featureHomeModule,
-            featureCounterModule,
+            testCommonModule,
         )
     }
+}
+
+private val testCommonModule = module {
+    includes(clientCounterModule, featureCounterModule)
 }
